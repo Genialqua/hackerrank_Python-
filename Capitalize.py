382b286983ddba27774b3642de8364ef8af1e502
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2025-02-22 23:18:21 +0100

    A function that prints a block

--- Capitalize.py
+++ Capitalize.py
@@ -1,12 +1,5 @@
-def solve(s):
-    # Split input string into separate words
-    words = s.split()
+def print_square(size):
+    for i in range(size):
+        print("#"*size)
 
-    # Capitalize the first letter of each word and join them back together
-    s = " ".join(word.capitalize() for word in words)
-
-    # Print the modified string
-    print(s)
-
-
-solve("hello world!")
+print_square(5)
